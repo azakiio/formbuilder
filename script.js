aae3f8b51aa8ad32a39725966999d0e072eb8e67
azakiio <azakica@gmail.com>
2020-01-08 17:38:36 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,77 @@
+var index = 1
+
+var tx = document.getElementsByTagName('textarea');
+for (var i = 0; i < tx.length; i++) {
+    //tx[i].setAttribute('style', 'height:' + (tx[i].scrollHeight) + 'px;overflow-y:hidden;');
+    tx[i].addEventListener("input", OnInput, false);
+}
+
+function OnInput() {
+    this.style.height = 'auto';
+    this.style.height = (this.scrollHeight) + 'px';
+}
+
+var coll = document.getElementsByClassName("collapsible");
+for (var i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
+
+
+function textAnswer(index, question) {
+    var form = document.getElementById("form")
+    var input = document.createElement("input");
+    input.type = "text";
+    input.name = index;
+    var label = document.createElement("label");
+    label.setAttribute("for", index);
+    label.innerText = index + ". " + question
+    form.appendChild(label)
+    form.appendChild(input)
+    form.innerHTML += "<br>"
+}
+
+function radioAnswer(index, question, answers) {
+    var form = document.getElementById("form");
+    var title = document.createElement("p");
+    title.innerText = index + ". " + question
+    form.appendChild(title)
+
+    var temp = answers.split(/\r?\n/);
+    for (a of temp) {
+        var input = document.createElement("input");
+        input.type = "radio";
+        input.name = index;
+        var label = document.createElement("label");
+        label.setAttribute("for", index);
+        label.innerText = a
+        form.appendChild(label)
+        form.appendChild(input)
+    }
+    form.innerHTML += "<br>"
+}
+
+function boxAnswer(index, question, answers) {
+    var form = document.getElementById("form");
+    var title = document.createElement("p");
+    title.innerText = index + ". " + question
+    form.appendChild(title)
+    for (a of answers) {
+        var input = document.createElement("input");
+        input.type = "checkbox";
+        input.name = index;
+        var label = document.createElement("label");
+        label.setAttribute("for", index);
+        label.innerText = a
+        form.appendChild(label)
+        form.appendChild(input)
+    }
+    form.innerHTML += "<br>"
+}
