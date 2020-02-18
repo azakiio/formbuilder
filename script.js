17fc856974fa29c7e74eb3c23545a98eae31a058
azakiio <azakica@gmail.com>
2020-02-18 06:40:36 -0500

    Updated Login Form and FormList

--- script.js
+++ script.js
@@ -12,34 +12,40 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
 var ref = database.ref("/form-list");
-ref.on('value', gotData, errData);
+// ref.on('value', gotData, errData);
 
 
 function gotData(data) {
     var forms = data.val();
     var keys = Object.keys(forms);
-    var ol = document.getElementById("formList")
-    ol.innerHTML = ""
+    var formList_div = document.getElementById('formList')
+    formList_div.innerHTML = ""
     for (var i = 0; i < keys.length; i++) {
         let k = keys[i];
-        var name = forms[k].name;
-        var li = document.createElement("li");
 
-        var link = document.createElement("a");
-        link.href = "/formbuilder/?k=" + k
+        let outer_div = document.createElement("div")
+        outer_div.classList.add("form-list-item")
 
-        var delbtn = document.createElement("img")
-        delbtn.src = "/assets/icons8-delete.svg"
-        delbtn.setAttribute("class", "delbtn")
-        delbtn.onclick = function(){
-            database.ref("/form-list/"+k).remove()
+        let name_p = document.createElement("p")
 
+        let link_a = document.createElement("a")
+        link_a.href = `/formbuilder/?k=${k}`
+        link_a.innerText = forms[k].name;
+
+        let date_p = document.createElement("p")
+        date_p.innerText = forms[k].creationDate
+
+        let delete_button = document.createElement("span")
+        delete_button.onclick = () => {
+            database.ref("/form-list/" + k).remove()
         }
+        delete_button.classList.add("del")
+        delete_button.innerText = "\u2715"
 
-        link.innerText = name
-        li.appendChild(link)
-        li.appendChild(delbtn)
-        ol.appendChild(li)
+        name_p.append(link_a)
+        outer_div.append(name_p, date_p, delete_button)
+        formList_div.append(outer_div)
+
     }
 }
 
@@ -50,29 +56,29 @@ function errData(err) {
 }
 
 
-firebase.auth().onAuthStateChanged(function(user) {
+firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
-      // User is signed in.
-  
-      document.getElementById("user_div").style.display = "block";
-      document.getElementById("login_div").style.display = "none";
-      ref.on('value', gotData, errData);
-      
-    } else {
-      // No user is signed in.
-  
-      document.getElementById("user_div").style.display = "none";
-      document.getElementById("login_div").style.display = "block";
-  
-    }
-  }); 
+        // User is signed in.
+        document.getElementById("logged-in").style.display = "block";
+        document.getElementById("logged-out").style.display = "none";
+        ref.on('value', gotData, errData);
 
-function newForm(formName) {
-    var data = {
-        name: formName,
-        content: ""
+    } else {
+        // No user is signed in.
+        document.getElementById("logged-in").style.display = "none";
+        document.getElementById("logged-out").style.display = "block";
+
     }
-    ref.push(data)
+});
+
+function newForm() {
+    var data = {
+        name: "Untitled",
+        content: "",
+        creationDate: new Date().toLocaleDateString()
+    }
+    var link = ref.push(data)
+    window.location.href = `/formbuilder/?k=${link.name}`
 }
 
 
@@ -87,6 +93,6 @@ function login() {
     });
 }
 
-function logout(){
+function logout() {
     firebase.auth().signOut();
-  }
+}
